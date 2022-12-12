f3619b78352ffb8a865049fe2f7385bca4f5d32d
User <lachesse@gmail.com>
2022-12-12 16:09:23 +0200

    code standarts for nodejs

--- nestjs/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-    CREATE DATABASE nestjs;
-    USE nestjs;
-    create user nestjsexample with encrypted password 'mypass';
-    grant all on database nestjs to nestjsexample;
-EOSQL
